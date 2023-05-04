c5b7181db59140f7b9bc960beaa8b1ad7b0cdc45
jorgeProgramaEn5 <jorgitoestrada2924@gmail.com>
2023-05-04 16:45:30 +0200

    modificate Card

--- src/base_datos.js
+++ src/base_datos.js
@@ -16,7 +16,7 @@ export const ciclas = [{
 },{
 	id: 1,
 	tipo: 'Ruta',
-	img:'https://cdn.deporvillage.com/cdn-cgi/image/h=576,w=576,dpr=1,f=auto,q=75,fit=contain,background=white/product/gst-74le1115-c_001.jpg',
+	img:'https://th.bing.com/th/id/OIP._qJwX019GFTsCeh_4sysRAHaFC?w=253&h=180&c=7&r=0&o=5&pid=1.7',
 	descripcion: 	`Las bicicleta MTB (Mountain Bike) 
 				  	es un tipo de bicicleta diseñada 
 					para ser utilizada en terrenos 
@@ -31,7 +31,7 @@ export const ciclas = [{
 },{
 	id: 2,
 	tipo: 'Paseo',
-	img:'https://cdn.deporvillage.com/cdn-cgi/image/h=576,w=576,dpr=1,f=auto,q=75,fit=contain,background=white/product/gst-74le1115-c_001.jpg',
+	img:'https://th.bing.com/th/id/OIP.Dp3qzFmjkoFbwvYvtDC45gHaHF?w=193&h=185&c=7&r=0&o=5&pid=1.7',
 	descripcion: 	`Las bicicleta MTB (Mountain Bike) 
 				  	es un tipo de bicicleta diseñada 
 					para ser utilizada en terrenos 
@@ -46,7 +46,7 @@ export const ciclas = [{
 },{
 	id: 3,
 	tipo: 'Plegable',
-	img:'https://cdn.deporvillage.com/cdn-cgi/image/h=576,w=576,dpr=1,f=auto,q=75,fit=contain,background=white/product/gst-74le1115-c_001.jpg',
+	img:'https://th.bing.com/th/id/OIP.KFER2LBqM7pCcHJVQGxrnQHaHa?w=196&h=196&c=7&r=0&o=5&pid=1.7',
 	descripcion: 	`Las bicicleta MTB (Mountain Bike) 
 				  	es un tipo de bicicleta diseñada 
 					para ser utilizada en terrenos 
