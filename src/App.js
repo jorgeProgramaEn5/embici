478a1d5e3c96f90b7df8a3f20c82de4d7a336259
jorgeProgramaEn5 <jorgitoestrada2924@gmail.com>
2023-05-06 16:18:51 +0200

    add baner

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+import React from 'react';
 import './estilos/App.css';
 import Display from './componentes/Display';
 import Boton from './componentes/Boton';
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>
-        Cantidad de km que deseas recorrer
-      </h1>
-      <Display />
-      <Boton text='Click' />
-      <Boton text='Reiniciar'/>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state= 0;
+  }
+
+
+  render() { 
+    return (
+      <div className="App">
+        <h1>
+          Cantidad de km que deseas recorrer
+        </h1>
+
+        <Display />
+
+        <Boton text='Click' />
+        
+        <Boton text='Reiniciar'/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -1,9 +1,17 @@
 import '../estilos/Boton.css';
 
 const Boton = (props)=>{
+
+  // const [contador, setContador] = useState(0);
+
+  // const pulseClick =()=>{
+  //   setContador(contador + 1);
+  //   // props.onContadorCambiado(contador + 1);
+  // }
+
   return(
     <div>
-      <div className='boton'>{props.text}</div>
+      <div className='boton' >{props.text}</div>
     </div>
   )
 }
